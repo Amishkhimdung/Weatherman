7348a584dc390160e3f683acb532020994203f35
Amish_Khimdung_Rai <np03cs4a220464@heraldcollege.edu.np>
2023-03-28 11:21:24 +0545

    first commit

---
+++ weather.js
@@ -0,0 +1,17 @@
+const apiKey = "6a675a69d5cb56ad2994d1356bb0dbe5";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const form = document.getElementById("form");
+
+form.addEventListener("click", async function (e) {
+    e.preventDefault();
+    const searchfield = document.getElementById("search-bar").value;
+    const response = await fetch(apiUrl + searchfield + `&appid=${apiKey}`);
+    const data = await response.json();
+    const cname = document.getElementById("cname");
+    cname.innerHTML = data.name;
+    const temp = document.getElementById("temp");
+    temp.innerHTML = Math.round(data.main.temp)+"°C";
+    const text = document.getElementById("condition");
+    text.innerHTML = data.weather[0].description;
+    console.log(data.weather[0].description);
+});
