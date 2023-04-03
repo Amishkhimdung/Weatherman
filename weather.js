5a2cc81d664fda7fde919c5c521cf0b223b706cb
Amish_Khimdung_Rai <np03cs4a220464@heraldcollege.edu.np>
2023-04-03 07:47:51 +0545

    fifth commit

--- weather.js
+++ weather.js
@@ -1,3 +1,7 @@
+var datetime = new Date();
+console.log(datetime);
+document.getElementById("time").textContent = datetime;
+
 const apiKey = "6a675a69d5cb56ad2994d1356bb0dbe5";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const form = document.getElementById("form");
@@ -14,4 +18,11 @@ form.addEventListener("click", async function (e) {
     const text = document.getElementById("condition");
     text.innerHTML = data.weather[0].description;
     console.log(data.weather[0].description);
+    const windspeed = document.getElementById("wind");
+    windspeed.innerHTML = data.wind.speed;
+    const humid = document.getElementById("humid");
+    humid.innerHTML = data.main.humidity;
+    const pressure = document.getElementById("press");
+    pressure.innerHTML = data.main.pressure;
+
 });
